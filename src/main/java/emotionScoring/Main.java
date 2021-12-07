e6d80f414319f1a404951d4ab0d8fd83d6c07e50
JoniLauk <jonilaukkanen1@gmail.com>
2021-12-07 15:41:04 +0200

    emotion

--- src/main/java/emotionScoring/Main.java
+++ src/main/java/emotionScoring/Main.java
@@ -1,7 +1,25 @@
 package emotionScoring;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        
+        List<Object> tunneSanat = new ArrayList<Object>(["Anger","Anticipation","Disgust","Fear","Joy","Sadness","Surprisem", "Trust"]);
+        String s = "lorem ipsum dolor sit amet";
+
+        List<String> myList = new ArrayList<String>(Arrays.asList(s.split(" ")));
+        System.out.println(myList);
+    }
+}
+
+class Emotion {
+    String word;
+    int count;
+
+    public Emotion(String word, int count){
+        word = word;
+        count = count;
     }
 }
--- src/main/java/emotionScoring/ReadFile.java
+++ src/main/java/emotionScoring/ReadFile.java
@@ -1,4 +1,4 @@
 package emotionScoring;
-
 public class ReadFile {
+
 }
